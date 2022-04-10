2952b1485449330f8be92596620528ef48d8745c
mooktar <mohmouktar@gmail.com>
2022-04-10 21:17:52 +0100

    configure tailwind css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pl-5 pr-5">
+      <h1 className='text-4xl mt-7 font-bold text-red-900 '>Rest Countires API</h1>
+      <p>
+        lorem ipsum dolor
+      </p>
     </div>
   );
 }
